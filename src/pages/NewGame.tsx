4009928ef607c91c3a8b05a0bd44b25252e6724a
mwyatt896 <mwyatt896@gmail.com>
2023-10-26 13:50:00 -0700

    fix QR code scanner

--- src/pages/NewGame.tsx
+++ src/pages/NewGame.tsx
@@ -1,56 +1,70 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { QrReader } from 'react-qr-reader';
+import qrImg from '../assets/bx-scan.svg';
 
 function NewGame() {
-    const [address, setAddress] = useState("");
-  
-    const handlePaste = () => {
-      navigator.clipboard.readText().then((clipboardData) => {
-        setAddress(clipboardData);
-      });
-    };
-  
-    return (
-      <div className="min-h-screen flex items-center justify-center bg-gray-100">
-        <div className="w-full max-w-xs mx-auto px-4 py-8 bg-white rounded-md shadow-md">
-          {/* Header */}
-          <div className="mb-4">
-            <h1 className="text-2xl font-bold text-center">Header</h1>
-          </div>
-  
-          {/* Circles */}
-          <div className="flex justify-between mb-4">
-            <div className="w-16 h-16 bg-orange-400 rounded-full"></div>
-            <div className="w-16 h-16 bg-orange-400 rounded-full"></div>
-          </div>
-  
-          {/* Challenge Text */}
-          <div className="mb-4">
-            <h2 className="text-xl text-center">Who could you like to challenge?</h2>
-          </div>
-  
-          {/* Input Field */}
-          <div className="mb-4">
-            <input 
-              type="text" 
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-              className="w-full p-2 border rounded-md"
-              placeholder="Enter Wallet Address"
-            />
-          </div>
-  
-          {/* Paste Button */}
-          <div className="mb-4">
-            <button 
-              onClick={handlePaste} 
-              className="w-full p-2 bg-gray-300 hover:bg-gray-400 rounded-md"
-            >
-              Paste from Clipboard
-            </button>
-          </div>
+  const [walletAddress, setWalletAddress] = useState<string>("");
+
+  const handleScan = (result: Result | null | undefined, error: Error | null | undefined) => {
+    if (result) {
+      setWalletAddress(result?.text);
+    }
+    if (error) {
+      console.info(error);
+    }
+  }
+
+  const handlePasteFromClipboard = async () => {
+    const clipboardData = await navigator.clipboard.readText();
+    setWalletAddress(clipboardData);
+  }
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <div className="max-w-xs w-full p-4 bg-white rounded">
+        <div className="flex justify-between mb-4">
+          <div className="w-8 h-8 border-2 border-black bg-orange-500 rounded-full"></div>
+          <div className="w-8 h-8 border-4 border-orange-500 rounded-full"></div>
+          <div className="w-8 h-8 border-4 border-orange-500 rounded-full"></div>
         </div>
+        <h1 className="text-xl font-bold mb-4">Who would you like to challenge?</h1>
+        <div className="relative mb-2">
+          <input
+            type="text"
+            className="w-full px-4 py-2 border rounded"
+            placeholder="Enter Wallet Address"
+            value={walletAddress}
+            readOnly
+          />
+          <button
+            onClick={() => {
+              // Open the camera to scan for QR codes.
+            }}
+            className="absolute right-1 top-1/2 transform -translate-y-1/2 bg-white border border-white rounded p-1"
+          >
+            <img src={qrImg} alt="QR Scanner" />
+          </button>
+        </div>
+        <button
+          onClick={handlePasteFromClipboard}
+          className="w-full py-2 mb-2 border rounded bg-gray-100 text-gray-700"
+        >
+          Paste from Clipboard
+        </button>
+        <QrReader
+          scanDelay={500}
+          onResult={handleScan}
+          className="w-full mb-2"
+          containerStyle={{ height: '240px' }}
+          constraints={{
+            facingMode: 'user'
+          }}
+        />
       </div>
-    );
+    </div>
+  );
 }
 
-export default NewGame
+export default NewGame;
+
+
