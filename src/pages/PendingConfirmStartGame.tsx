bccd981734a70b6501c4b8f33ae1c6e71dee999c
mwyatt896 <mwyatt896@gmail.com>
2023-10-27 16:49:49 -0700

    updating to new UI

--- src/pages/PendingConfirmStartGame.tsx
+++ src/pages/PendingConfirmStartGame.tsx
@@ -1,51 +1,168 @@
-import behindBuildingImg from '../assets/behind_building.svg';
-import inWeedsImg from '../assets/in_weeds.svg';
-
 function PendingConfirmStartGame() {
 
     return (
-        <div className="min-h-screen flex justify-center items-center bg-gray-100">
-            <div className="max-w-xs w-full p-6 bg-white rounded-md shadow-md">
-                <h1 className="text-2xl font-bold mb-4">Review & Confirm</h1>
-
-                <div className="flex justify-center items-center mb-4"> {/* Added justify-center */}
-                    <div className="w-20 h-10 bg-gray-300 rounded-full mr-4"> {/* Increased width */}
-                    <span className="text-lg">Game x123</span>
-                    </div>
+        <div className="items-center bg-neutral-900 flex w-[390px] flex-col pl-5 pr-5">
+          <div className="bg-neutral-900 self-stretch flex w-full flex-col mt-8 mb-20 pl-1 max-md:ml-px max-md:mb-10">
+            <div className="self-stretch flex w-full items-start justify-between gap-5">
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/cd84c866-46d9-4d71-af0b-2055777b3fcb?"
+                className="aspect-[2.95] object-cover object-center w-[115px] fill-white overflow-hidden self-stretch max-w-full"
+              />
+              <div className="border-[color:var(--White,#FCFCFC)] self-stretch flex w-[155px] max-w-full flex-col px-5 py-4 rounded-[200px] border-2 border-solid">
+                <div className="text-zinc-50 text-center text-xs font-bold self-center whitespace-nowrap">
+                  aleo7899...3456
                 </div>
-
-                <div className="flex justify-center items-center mb-6"> {/* Added justify-center */}
-                    <div className="w-16 h-8 bg-gray-300 rounded-full mr-2"> {/* Increased width */}
-                        <span>Bob</span>
-                    </div>
-                    <span className="mx-2 text-black">vs</span>
-                    <div className="w-16 h-8 bg-blue-500 rounded-full mr-2"> {/* Increased width */}
-                        <span>Alice</span>
-                    </div>
-                </div>
-
-                <h2 className="text-xl mb-4 text-black text-center">Hid Alex in:</h2>
-
-                <div className="grid grid-cols-2 gap-4 mb-6">
-                    <div className="relative">
-                        <img src={inWeedsImg} alt="In Weeds" className="rounded-full w-full h-32 object-cover"/>
-                    </div>
-                    <img src={behindBuildingImg} alt="Behind Building" className="rounded-full w-full h-32 object-cover"/>
-                </div>
-
-                <div className="mb-4 text-center">
-                    <span className="text-lg text-black">Wager:</span>
-                    <div className="w-full h-12 bg-gray-200 rounded mt-2 flex items-center justify-center">
-                        <span>10 Puzzle Pieces</span>
-                    </div>
-                </div>
-
-                <button className="w-full bg-green-500 text-white p-3 rounded">Kickoff Game!</button>
-
-                <button className="w-full mt-4 p-3 text-center">Back</button>
+              </div>
             </div>
+            <div className="text-lime-600 text-right text-base font-bold border-[color:var(--Green,#4EC331)] self-stretch w-full pl-0 mt-8 pr-2.5 py-3.5 border-2 border-solid max-md:mr-1">
+              000000000010
+              <br />
+              Puzzle Pieces
+            </div>
+            <div className="bg-yellow-300 self-stretch flex w-full flex-col mt-7 px-5 py-8 rounded-[200px] max-md:mr-1.5">
+              <div className="text-black text-center text-4xl font-extrabold self-center w-full">
+                NEW GAME
+              </div>
+            </div>
+            <div className="border-[color:var(--Pink,#FFAED5)] self-stretch flex w-full flex-col mt-8 pr-5 pb-5 border-2 border-solid max-md:mr-1.5">
+              <div className="bg-pink-300 flex w-[139px] max-w-full flex-col px-5 py-2 self-start">
+                <div className="text-neutral-900 text-center text-xs font-extrabold leading-3 self-center whitespace-nowrap">
+                  NOTIFICATIONS
+                </div>
+              </div>
+              <div className="items-start flex w-full flex-col ml-4 mt-4 pl-px self-start max-md:ml-2.5">
+                <div className="justify-between items-start self-stretch flex w-full gap-5 max-md:justify-center">
+                  <div className="text-pink-300 text-center text-xs font-bold tracking-tight self-center my-auto">
+                    Alice
+                  </div>
+                  <div className="text-pink-300 text-center text-xs font-bold tracking-tight self-center my-auto">
+                    10 Puzzle
+                  </div>
+                  <div className="text-black text-center text-xs font-extrabold bg-lime-600 self-stretch w-[95px] max-w-full px-5 py-3 rounded-[200px]">
+                    CLAIM
+                  </div>
+                </div>
+                <div className="justify-between items-start self-stretch flex w-full gap-5 mt-4 max-md:justify-center">
+                  <div className="text-pink-300 text-center text-xs font-bold tracking-tight self-center my-auto">
+                    Alice
+                  </div>
+                  <div className="text-pink-300 text-center text-xs font-bold tracking-tight self-center my-auto">
+                    10 Puzzle
+                  </div>
+                  <div className="text-black text-center text-xs font-extrabold bg-yellow-300 self-stretch w-[95px] max-w-full px-5 py-3 rounded-[200px]">
+                    FINISH
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="border-[color:var(--Red,#F63B3B)] self-stretch flex w-full flex-col mt-6 pr-5 pb-5 border-2 border-solid max-md:mr-1.5">
+              <div className="bg-red-500 flex w-[139px] max-w-full flex-col px-5 py-2 self-start">
+                <div className="text-neutral-900 text-center text-xs font-extrabold leading-3 self-center whitespace-nowrap">
+                  LIVE GAMES
+                </div>
+              </div>
+              <div className="items-start flex w-full flex-col ml-5 mt-3 self-start max-md:ml-2.5">
+                <div className="justify-between items-start self-stretch flex gap-1.5 max-md:justify-center">
+                  <div className="text-red-500 text-center text-xs font-bold self-center my-auto">
+                    Alice
+                  </div>
+                  <div className="text-red-500 text-center text-xs font-bold self-center my-auto">
+                    10 Puzzle
+                  </div>
+                  <div className="text-neutral-200 text-center text-xs font-extrabold self-stretch bg-zinc-500 bg-opacity-20 w-[79px] max-w-full px-4 py-3 rounded-[200px] whitespace-nowrap">
+                    1:34:34
+                  </div>
+                  <div className="text-black text-center text-xs font-extrabold self-stretch bg-pink-300 w-[58px] max-w-full px-4 py-3 rounded-[200px] whitespace-nowrap">
+                    PING
+                  </div>
+                </div>
+                <div className="justify-between items-start self-stretch flex gap-1.5 mt-4 max-md:justify-center">
+                  <div className="text-red-500 text-center text-xs font-bold self-center my-auto">
+                    Alice
+                  </div>
+                  <div className="text-red-500 text-center text-xs font-bold self-center my-auto">
+                    10 Puzzle
+                  </div>
+                  <div className="text-black text-center text-xs font-extrabold self-stretch bg-zinc-500 w-[79px] max-w-full pl-5 pr-5 py-3 rounded-[200px] whitespace-nowrap max-md:pl-px">
+                    RENEG
+                  </div>
+                  <div className="text-black text-center text-xs font-extrabold self-stretch bg-pink-300 w-[58px] max-w-full px-4 py-3 rounded-[200px] whitespace-nowrap">
+                    PING
+                  </div>
+                </div>
+                <div className="justify-between items-start self-stretch flex gap-1.5 mt-4 max-md:justify-center">
+                  <div className="text-red-500 text-center text-xs font-bold self-center my-auto">
+                    Alice
+                  </div>
+                  <div className="text-red-500 text-center text-xs font-bold self-center my-auto">
+                    10 Puzzle
+                  </div>
+                  <div className="text-black text-center text-xs font-extrabold self-stretch bg-zinc-500 w-[79px] max-w-full pl-5 pr-5 py-3 rounded-[200px] whitespace-nowrap max-md:pl-px">
+                    RENEG
+                  </div>
+                  <div className="text-black text-center text-xs font-extrabold self-stretch bg-pink-300 w-[58px] max-w-full px-4 py-3 rounded-[200px] whitespace-nowrap">
+                    PING
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="border-[color:var(--Grey,#868686)] self-stretch flex w-full grow flex-col mt-5 pr-5 pb-5 border-2 border-solid max-md:mr-1.5">
+              <div className="bg-zinc-500 flex w-[139px] max-w-full flex-col px-5 py-2 self-start">
+                <div className="text-neutral-900 text-center text-xs font-extrabold leading-3 self-center">
+                  PAST GAMES
+                </div>
+              </div>
+              <div className="items-start flex w-full flex-col ml-5 mt-3.5 self-start max-md:ml-2.5">
+                <div className="justify-between items-start self-stretch flex w-full gap-5 max-md:justify-center">
+                  <div className="text-zinc-500 text-center text-xs font-bold self-center my-auto">
+                    Alice
+                  </div>
+                  <div className="text-zinc-500 text-center text-xs font-bold self-center my-auto">
+                    10 Puzzle
+                  </div>
+                  <div className="text-black text-center text-xs font-extrabold self-stretch bg-zinc-500 w-[67px] max-w-full px-5 py-3 rounded-[200px] whitespace-nowrap">
+                    WON
+                  </div>
+                </div>
+                <div className="justify-between items-start self-stretch flex w-full gap-5 mt-4 max-md:justify-center">
+                  <div className="text-zinc-500 text-center text-xs font-bold self-center my-auto">
+                    Alice
+                  </div>
+                  <div className="text-zinc-500 text-center text-xs font-bold self-center my-auto">
+                    10 Puzzle
+                  </div>
+                  <div className="text-black text-center text-xs font-extrabold self-stretch bg-zinc-500 w-[67px] max-w-full pl-5 pr-5 py-3 rounded-[200px] whitespace-nowrap">
+                    LOST
+                  </div>
+                </div>
+                <div className="justify-between items-start self-stretch flex w-full gap-5 mt-4 max-md:justify-center">
+                  <div className="text-zinc-500 text-center text-xs font-bold self-center my-auto">
+                    Alice
+                  </div>
+                  <div className="text-zinc-500 text-center text-xs font-bold self-center my-auto">
+                    10 Puzzle
+                  </div>
+                  <div className="text-black text-center text-xs font-extrabold bg-zinc-500 self-stretch w-[67px] max-w-full px-4 py-3 rounded-[200px]">
+                    DRAW
+                  </div>
+                </div>
+                <div className="justify-between items-start self-stretch flex w-full gap-5 mt-4 max-md:justify-center">
+                  <div className="text-zinc-500 text-center text-xs font-bold self-center my-auto">
+                    Alice
+                  </div>
+                  <div className="text-zinc-500 text-center text-xs font-bold self-center my-auto">
+                    10 Puzzle
+                  </div>
+                  <div className="text-black text-center text-xs font-extrabold self-stretch bg-zinc-500 w-[67px] max-w-full px-2.5 py-3 rounded-[200px] whitespace-nowrap">
+                    CANCELED
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      );
 }
 
 export default PendingConfirmStartGame;
