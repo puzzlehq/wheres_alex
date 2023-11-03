0cfc6d1e44b029f36510e97082f5191bd245e73a
dark horse <asharma13524@gmail.com>
2023-11-02 21:47:25 -0400

    Add in puzzle sdk and slight tweaks, lots of cleanup todo

--- src/App.tsx
+++ src/App.tsx
@@ -13,26 +13,29 @@ import rightImageSrc from '../src/assets/alex_mic_left_tilt.png';
 import leftImageSrc from '../src/assets/alex_mic_right_tilt.png';
 import bottomImageSrc from '../src/assets/alexbottom.png';
 import GameStarted from './pages/GameStarted.js';
+import { useConnect } from '@puzzlehq/sdk';
 
 function Welcome({ onConnectWallet }: { onConnectWallet: () => void }) {
+  const { connect, isConnected, loading } = useConnect();
+
   return (
     <div className="h-screen flex w-full justify-between items-stretch">
       <div className="relative flex w-full flex-col justify-center items-center h-full">
 
         <img src={rightImageSrc} alt="Right top decoration" className="absolute top-0 right-0 h-full max-w-[50%] max-h-[18rem] object-contain z-0" />
-        
+
         <img src={leftImageSrc} alt="Left decoration" className="absolute top-1/4 left-0 h-full max-w-[80%] max-h-[20rem] object-contain transform -translate-x-1/4 -translate-y-20 w-3/5 z-0" />
-        
+
         <h1 className="z-5 text-white text-center text-24xl font-extrabold leading-[40.56px] tracking-tight whitespace-nowrap max-w-full overflow-visible">
             WHERE <br /> IS ALEX?{" "}
         </h1>
-        <p 
+        <p
           className="z-10 text-white text-center text-base font-bold tracking-tight max-w-[400px] mt-8"
         >
             A thrilling game showcasing the power of Aleo and the Puzzle multiparty privacy stack through a wager between friends!
         </p>
-        <button 
-            onClick={onConnectWallet} 
+        <button
+            onClick={connect}
             className="z-10 bg-yellow-300 flex justify-center items-center mt-7 px-5 py-8 rounded-[200px] text-black text-4xl font-extrabold w-1/2 hover:bg-yellow-400"
         >
             Play!
@@ -77,28 +80,28 @@ function App() {
   };
   console.log(account.address);
 
-  const onConnectWallet = () => { 
+  const onConnectWallet = () => {
     setIsConnected(true);
   };
 
   return (
     <div className="App min-h-screen bg-yellow-500 flex justify-center">
       <div className="max-w-screen-sm w-full bg-neutral-900 shadow-md overflow-y-auto flex flex-col">
-        
+
         {isConnected && account?.address && <Header isConnected={isConnected} address={account?.address} />}
-        
+
         <Router>
           <div className="max-w-screen-sm w-full h-[calc(100vh-4rem)]">
             <Routes>
-              <Route path="/new-game" element={<NewGame />} /> 
-              <Route path="/start-wager" element={<StartWager />} /> 
+              <Route path="/new-game" element={<NewGame />} />
+              <Route path="/start-wager" element={<StartWager />} />
               <Route path="/hide-alex" element={<HideAlex />} />
               <Route path="/confirm-start-game" element={<ConfirmStartGame account={account}/>} />
               <Route path='/game-started' element={<GameStarted />} />
-              <Route path="/pending-confirm-start-game" element={<PendingConfirmStartGame />} /> 
-              <Route 
-                path="/" 
-                element={ isConnected ? <Home /> : <Welcome onConnectWallet={onConnectWallet} /> } 
+              <Route path="/pending-confirm-start-game" element={<PendingConfirmStartGame />} />
+              <Route
+                path="/"
+                element={ isConnected ? <Home /> : <Welcome onConnectWallet={onConnectWallet} /> }
               />
             </Routes>
           </div>
