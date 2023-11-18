a03d4b44946aaca4827eda957aab3e1fc6e9598f
Luke Newman <5315645+lukenewman@users.noreply.github.com>
2023-11-18 03:28:08 +0300

    use data rather than regex

--- src/state/usePieces.ts
+++ src/state/usePieces.ts
@@ -33,7 +33,7 @@ export const usePieces = () => {
       const totalBalance = records
         .filter(record => !record.spent)
         .map(record => {
-          const amount = record.plaintext.match(/amount:(\d+)u64/);
+          const amount = record.data.amount;
           if (amount) {
             /// find largestPiece (and thus availableBalance)
             const amountInt = parseInt(amount[1]);
