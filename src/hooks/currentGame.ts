8181fe1879c21c4f94742c65617daec3aad9f80c
jonandgon <jag73155@usc.edu>
2023-12-05 22:15:44 -0500

    add images

--- src/hooks/currentGame.ts
+++ src/hooks/currentGame.ts
@@ -1,9 +1,10 @@
 import { useGameStore } from '@state/gameStore';
 import { useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 
 export const useInitCurrentGame = () => {
   const navigate = useNavigate();
+  const location = useLocation();
   const { game_multisig } = useParams();
 
   const [yourTurn, theirTurn, currentGame, setCurrentGame] = useGameStore(
@@ -23,7 +24,7 @@ export const useInitCurrentGame = () => {
           game.gameNotification.recordData.game_multisig === game_multisig
       );
       _currentGame && setCurrentGame(_currentGame);
-    } else if (!game_multisig) {
+    } else if (!game_multisig && location.pathname !== '/new-game') {
       navigate('/');
     }
   }, [game_multisig, currentGame, [...yourTurn, ...theirTurn].toString()]);
